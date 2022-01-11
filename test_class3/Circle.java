d9b75fdac6729bede69a093513d863ebd04fca8e
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-01-11 10:42:16 +0900

    22.01.05

---
+++ test_class3/Circle.java
@@ -0,0 +1,29 @@
+package test_class3;
+
+public class Circle {
+
+	private int r;
+	private double circum, area;
+	
+	public int getR() {
+		return r;
+	}
+	public void setR(int r) {
+		this.area = r;
+	}
+	public double getCircum() {
+		return circum;
+	}
+	public double getArea() {
+		return area;
+	}
+	
+	public void calcCircum() {
+		circum = 2 * Math.PI * r;
+	}
+	
+	public void calcArea() {
+		area = Math.PI + Math.pow(r, 2);
+	}
+
+}

65ab75c1f3cc5276ba6cf3af67a0f26a570f1d6c
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-01-12 09:07:17 +0900

    22.01.07

---
+++ test_this1/ThisTest1.java
@@ -0,0 +1,21 @@
+package test_this1;
+
+public class ThisTest1 {
+
+	private int n;
+	
+	public void power(int n) {
+		n = n * n;
+		System.out.printf("%dÀÇ Á¦°ö½ÂÀº %dÀÌ´Ù.\n", n, this.n);
+	}
+	
+	public int power(int x, int y) {
+		n = 1;
+		
+		for(int i=0; i<y; i++) {
+			n *= x;
+		}
+		
+		return n;
+	}
+}

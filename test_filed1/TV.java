237182247a53a95ce81d915e4daadf682a47953a
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-01-13 23:29:23 +0900

    22.01.10

---
+++ test_filed1/TV.java
@@ -0,0 +1,20 @@
+package test_filed1;
+
+public class TV {
+	String color;
+	int size;
+	public static String PORODUCER = "LGÀüÀÚ";
+	
+	public String getColor() {
+		return color;
+	}
+	public int getSize() {
+		return size;
+	}
+	
+	public TV(String color, int size) {
+		this.color = color;
+		this.size = size;
+	}
+	
+}
